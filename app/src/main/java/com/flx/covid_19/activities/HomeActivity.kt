50fedc30da0fcdf9f28e14094cbb4d9e3cf976ea
ODUOR FELIX <foduor@ngamia.africa>
2020-08-13 15:31:14 +0300

    Dashboard UI Design

--- app/src/main/java/com/flx/covid_19/activities/HomeActivity.kt
+++ app/src/main/java/com/flx/covid_19/activities/HomeActivity.kt
@@ -1,7 +1,13 @@
 package com.flx.covid_19.activities
 
+import android.graphics.PorterDuff
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 import com.flx.covid_19.R
 
 class HomeActivity : AppCompatActivity() {
@@ -9,6 +15,31 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        
+        initToolbar()
+    }
+
+    private fun initToolbar() {
+        val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
+        toolbar.setNavigationIcon(R.drawable.ic_menu)
+        toolbar.navigationIcon!!.setColorFilter(resources.getColor(R.color.grey_60), PorterDuff.Mode.SRC_ATOP)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        //Tools.setSystemBarColor(this, R.color.grey_5)
+        //Tools.setSystemBarLight(this)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_search_setting, menu)
+        //Tools.changeMenuIconColor(menu, resources.getColor(R.color.grey_60))
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+        } else {
+            Toast.makeText(applicationContext, item.title, Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
